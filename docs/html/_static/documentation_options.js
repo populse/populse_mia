5813af2f2015f1fe6c0b9c23897e53a209703ff5
econdami <econdami@yahoo.fr>
2020-11-17 19:22:04 +0100

    update docstring in populse_mia.software_properties.Config

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0-dev+31a83e23',
+    VERSION: '1.4.0-dev+2a47d32a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
