54603f6043de032f45d75c90dc7fe90e2771692d
econdami <econdami@yahoo.fr>
2020-12-17 15:41:23 +0100

    iteration documentation in v1.x

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0-dev+c71a7bf1',
+    VERSION: '1.4.0-dev+6651be7b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
