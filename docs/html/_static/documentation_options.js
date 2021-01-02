ff027ccd7350de03e78293260d02869314000015
econdami <econdami@yahoo.fr>
2021-01-02 12:46:46 +0100

    upadate documentation

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0-dev+2a47d32a',
+    VERSION: '1.4.0-dev+761d3b81',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
