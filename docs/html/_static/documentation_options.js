f439b06da14bba32af98ad479d3c51113efc50b3
econdami <econdami@yahoo.fr>
2021-01-25 09:38:33 +0100

    documentation update

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,11 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0-dev+44a2feab',
+    VERSION: '1.4.0-dev+e8f72c52',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
