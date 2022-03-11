c3b29ed541a8d30e070beaa9372860745e5bcf7b
econdami <econdami@yahoo.fr>
2022-03-11 12:12:56 +0100

    ticket #191, cleaning of info.py

--- python/populse_mia/info.py
+++ python/populse_mia/info.py
@@ -1,5 +1,12 @@
-# -*- coding: utf-8 -*- # Character encoding, recommended
 """Define software version, description and requirements"""
+
+###############################################################################
+# Populse_mia - Copyright (C) IRMaGe/CEA, 2018
+# Distributed under the terms of the CeCILL license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html
+# for details.
+###############################################################################
 
 import os
 import subprocess
@@ -9,25 +16,31 @@ import sys
 version_major = 2
 version_minor = 0
 version_micro = 0
-version_extra = "dev" # leave empty for release
+version_extra = "dev"  # leave empty for release
 
 # Expected by setup.py: string of form "X.Y.Z"
 if version_extra:
-    __version__ = "{0}.{1}.{2}-{3}".format(version_major, version_minor, version_micro, version_extra)
-    
+    __version__ = "{0}.{1}.{2}-{3}".format(version_major, version_minor,
+                                           version_micro, version_extra)
+
 else:
-    __version__ = "{0}.{1}.{2}".format(version_major, version_minor, version_micro)
-    
+    __version__ = "{0}.{1}.{2}".format(version_major, version_minor,
+                                       version_micro)
+
+
 def get_populse_mia_gitversion():
     """Mia version as reported by the last commit in git
-    Returns the version or None if nothing was found
+    
+    :returns: the version or None if nothing was found
     """
+
     try:
         import populse_mia
         dir_mia = os.path.realpath(
-            os.path.join(os.path.dirname(populse_mia.__file__), os.path.pardir, os.path.pardir))
+            os.path.join(os.path.dirname(populse_mia.__file__),
+                         os.path.pardir, os.path.pardir))
 
-    except:
+    except Exception:
         dir_mia = os.getcwd()
 
     dir_miagit = os.path.join(dir_mia, ".git")
@@ -38,9 +51,9 @@ def get_populse_mia_gitversion():
     ver = None
 
     try:
-        gitversion, _ = subprocess.Popen(
-            "git show -s --format=%h", shell=True, cwd=dir_mia, stdout=subprocess.PIPE
-        ).communicate()
+        gitversion, _ = subprocess.Popen("git show -s --format=%h",
+                                         shell=True, cwd=dir_mia,
+                                         stdout=subprocess.PIPE).communicate()
 
     except Exception:
         pass
@@ -49,6 +62,7 @@ def get_populse_mia_gitversion():
         ver = gitversion.decode().strip().split("-")[-1]
 
     return ver
+
 
 if __version__.endswith("-dev"):
     gitversion = get_populse_mia_gitversion()
@@ -59,7 +73,8 @@ if __version__.endswith("-dev"):
 # Expected by setup.py: the status of the project
 CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
                'Intended Audience :: Developers',
-               'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+               'License :: OSI Approved :: CEA CNRS Inria '
+               'Logiciel Libre License, version 2.1 (CeCILL-2.1)',
                'Topic :: Software Development :: Libraries :: Python Modules',
                'Operating System :: OS Independent',
                'Programming Language :: Python :: 3.5',
@@ -76,7 +91,7 @@ LONG_DESCRIPTION = """
 ===============
 populse_mia
 ===============
-[MIA] Multiparametric Image Analysis:
+[MIA] Multi parametric Image Analysis:
 A complete image processing environment mainly targeted at 
 the analysis and visualization of large amounts of MRI data
 """
