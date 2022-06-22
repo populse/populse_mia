f466d9b923358caad374fee887211c27827a4d8b
patafana <otaviodcorrea@gmail.com>
2022-06-22 16:51:38 -0300

    Debug AppVeyor - Visual Studio 2019 build.

--- python/populse_mia/test.py
+++ python/populse_mia/test.py
@@ -5534,8 +5534,109 @@ class TestMIAPipelineManagerTab(unittest.TestCase):
         # FIXME: the above call to the function leads to a Segmentation
         # fault when the test routine is lauched in AppVeyor.
 
-    @unittest.skip('triggers core dumped error')
+    @unittest.skip('triggers segmentation fault error')
     def test_finish_execution(self):
+        '''
+        Mocks several objects of the pipeline manager and finishes the 
+        execution of the pipeline.
+
+        Notes
+        -----
+        Tests the method PipelineManagerTab.finish_execution.
+        '''
+
+        # Sets shortcuts for objects that are often used
+        ppl_manager = self.main_window.pipeline_manager
+        ppl_edt_tabs = ppl_manager.pipelineEditorTabs
+        ppl = ppl_edt_tabs.get_current_pipeline()
+
+        # Gets the path of one document
+        config = Config(config_path=self.config_path)
+        folder = os.path.abspath(os.path.join(config.get_mia_path(),
+                                              'resources', 'mia', 'project_8',
+                                              'data', 'raw_data'))
+
+        NII_FILE_1 = ('Guerbet-C6-2014-Rat-K52-Tube27-2014-02-14102317-01-G1_'
+                      'Guerbet_Anat-RAREpvm-000220_000.nii')
+
+        DOCUMENT_1 = os.path.abspath(os.path.join(folder, NII_FILE_1))
+        
+        # Adds a Rename processes, creates the 'rename_1' node
+        ppl_edt_tabs.get_current_editor().click_pos = QPoint(450, 500)
+        ppl_edt_tabs.get_current_editor().add_named_process(Rename)
+
+        ppl_edt_tabs.get_current_editor().export_unconnected_mandatory_inputs()
+        ppl_edt_tabs.get_current_editor().export_all_unconnected_outputs()
+        
+        # Sets the mandatory parameters
+        ppl.nodes[''].set_plug_value('in_file', DOCUMENT_1)
+        ppl.nodes[''].set_plug_value('format_string', 'new_name.nii')
+
+        ppl_manager.runPipeline()
+
+        # Mock objects of 'pipeline_manager' used during the test
+        ppl_manager.progress = Mock()
+        ppl_manager.progress.worker.status = 'status_value'
+        ppl_manager.last_run_pipeline = Mock()
+        ppl_manager.last_pipeline_name = 'last_pipeline_name_value'
+        ppl_manager._mmovie = Mock()
+
+        # Mock methods of 'ppl_manager' used during the test
+        ppl_manager.main_window.statusBar().showMessage = MagicMock()
+        ppl_manager.show_pipeline_status_action.setIcon = MagicMock()
+        ppl_manager.nodeController.update_parameters = MagicMock()
+        ppl_manager.run_pipeline_action.setDisabled = MagicMock()
+        ppl_manager.garbage_collect_action.setDisabled = MagicMock()
+        ppl_manager.stop_pipeline_action.setEnabled = MagicMock()
+
+        # Connect 'worker' to 'finished_execution' method
+        #(ppl_manager.progress.worker.finished.
+        #                                  connect(ppl_manager.finish_execution))
+
+        # Finish the execution of the pipeline (no errors are thrown)
+        #ppl_manager.finish_execution()
+        # TODO: fix 'core dumped' error, likely triggered by the above 
+        # line
+
+        # Asserts that the mocked objects were called as expected
+        #self.assertEqual('status_value', ppl_manager.last_status)
+        #self.assertFalse(hasattr(ppl_manager, '_mmovie'))
+        #self.assertIsNone(ppl_manager.last_run_log)
+
+        # Asserts that the mocked methods were called as expected
+        #(ppl_manager.stop_pipeline_action.
+        #                              setEnabled.assert_called_once_with(False))
+        #(ppl_manager.last_run_pipeline.get_study_config().
+        #                           engine.raise_for_status.assert_called_once())
+        #(ppl_manager.main_window.statusBar().showMessage.
+        #                                               assert_called_once_with)(
+        #                        'Pipeline "{0}" has been correctly run.'.format(
+        #                                        ppl_manager.last_pipeline_name))
+        #ppl_manager.show_pipeline_status_action.setIcon.assert_called_once()
+        #ppl_manager.nodeController.update_parameters.assert_called_once_with()
+        #(ppl_manager.run_pipeline_action.
+        #                             setDisabled.assert_called_once_with(False))
+        #(ppl_manager.garbage_collect_action.
+        #                             setDisabled.assert_called_once_with(False))
+
+        # Mock objects in order to induce a 'RuntimeError' which is 
+        # treated by the method
+        ppl_manager.progress = Mock()
+        ppl_manager.progress.worker.status = swconstants.WORKFLOW_DONE
+        delattr(ppl_manager.progress.worker, 'exec_id')
+        ppl_manager._mmovie = Mock()
+
+        # Finish the execution of the pipeline with no 'worker.exec_id' 
+        # (an error is thrown)
+        ppl_manager.finish_execution()
+
+        # Asserts that the execution of the pipeline failed
+        self.assertEqual(ppl_manager.last_run_log, 
+                         'Execution aborted before running')
+
+    
+    @unittest.skip('triggers core dumped error')
+    def test_finish_execution1(self):
         '''
         Mocks several objects of the pipeline manager and finishes the 
         execution of the pipeline.
