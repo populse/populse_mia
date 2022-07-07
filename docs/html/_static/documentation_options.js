3ef9de29b3b4ebd1869e8a89467c584630225c19
YannNdr <adebiyi.sadio.ndour@gmail.com>
2022-07-07 11:43:59 +0200

    making some pictures appear in virtualisation_pre_req_windows10.rst

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+d8a08fada',
+    VERSION: '2.2.1-dev+e0fe4da89',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
