18d2748459bdae9abbdbe30951870542bf43f706
YannNdr <adebiyi.sadio.ndour@gmail.com>
2022-07-07 12:04:36 +0200

    update changes for virtualisation_pre_req_windows10

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+e0fe4da89',
+    VERSION: '2.2.1-dev+12a739a2d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
