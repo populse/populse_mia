5b01c51808ca94d36dbe58b77e9533150c2679de
YannNdr <adebiyi.sadio.ndour@gmail.com>
2022-07-07 10:39:44 +0200

    updating virtualisation_pre_req_windows10

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+7b35552a2',
+    VERSION: '2.2.1-dev+b06d92590',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
