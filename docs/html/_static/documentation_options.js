44be504111eff9c516d945c87e8ab3bbf0bf53b9
YannNdr <adebiyi.sadio.ndour@gmail.com>
2022-07-08 10:16:08 +0200

    making pictures display from the html documentation

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+12a739a2d',
+    VERSION: '2.2.1-dev+18d274845',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
