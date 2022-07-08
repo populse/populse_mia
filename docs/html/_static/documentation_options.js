fd4816e74ec33efc81f497f25dcb5da7ca6cd78d
YannNdr <adebiyi.sadio.ndour@gmail.com>
2022-07-08 11:18:54 +0200

    adding files with supposed working filename in rst file

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+718ab2b8c',
+    VERSION: '2.2.1-dev+09369a2bb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
