c434ab6e4d408e0662da2be6b7e7171895bec395
patafana <otaviodcorrea@gmail.com>
2022-07-20 20:10:06 -0300

    Try to fix build error.

--- python/populse_mia/test.py
+++ python/populse_mia/test.py
@@ -176,16 +176,12 @@ from populse_mia.user_interface.pop_ups import (DefaultValueListCreation,
 from populse_mia.utils.utils import check_value_type, table_to_database
 
 # populse_db import
-from populse_db.database import (FIELD_TYPE_BOOLEAN, FIELD_TYPE_DATE,
-                                 FIELD_TYPE_DATETIME, FIELD_TYPE_INTEGER,
-                                 FIELD_TYPE_STRING, FIELD_TYPE_TIME,
-                                 FIELD_TYPE_LIST_BOOLEAN, 
-                                 FIELD_TYPE_LIST_DATE, 
-                                 FIELD_TYPE_LIST_DATETIME, 
-                                 FIELD_TYPE_LIST_FLOAT, 
-                                 FIELD_TYPE_LIST_INTEGER, 
-                                 FIELD_TYPE_LIST_STRING, 
-                                 FIELD_TYPE_LIST_TIME)
+from populse_db.database import(FIELD_TYPE_STRING, FIELD_TYPE_FLOAT, 
+                                FIELD_TYPE_DATETIME, FIELD_TYPE_DATE, 
+                                FIELD_TYPE_TIME, FIELD_TYPE_LIST_DATE,
+                                FIELD_TYPE_LIST_DATETIME, FIELD_TYPE_LIST_TIME, 
+                                FIELD_TYPE_LIST_INTEGER, FIELD_TYPE_LIST_STRING, 
+                                FIELD_TYPE_LIST_FLOAT, FIELD_TYPE_LIST_BOOLEAN)
 
 # soma import
 from soma.qt_gui.qt_backend.Qt import QTreeView
