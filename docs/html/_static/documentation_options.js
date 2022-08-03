10e3ae48eded5dc61045517c357e0b227a4255c1
econdami <econdami@yahoo.fr>
2022-08-03 18:27:17 +0200

    make html documentation (just to see the current result)

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+e4dcd310b',
+    VERSION: '2.2.1-dev+08ef7be8e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
