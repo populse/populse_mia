efb4fe8ce7d2ad363ed1f789cfba84b45707aa81
Yann NDOUR <adebiyi.sadio.dnour@gmail.com>
2022-08-26 12:22:53 +0200

    make html file to update webpage

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.1-dev+0e6da2bac',
+    VERSION: '2.2.1-dev+88d0dc98d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
