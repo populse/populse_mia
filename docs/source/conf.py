cca05930eb5d40be399349459e435cc2b5044800
Denis Rivière <denis.riviere.gm@gmail.com>
2022-09-13 17:32:12 +0200

    doc for remote computing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -133,7 +133,15 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks",
 ]
+
+try:
+    extensions.append("sphinxcontrib.mermaid")
+except ImportError:
+    # oh well... no schemas then.
+    pass
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -285,3 +293,30 @@ epub_exclude_files = ["search.html"]
 autodoc_default_options = {
     "special-members": "__init__",
 }
+
+
+# -- Intersphinx configuration -----------------------------------------------
+
+extlinks = {
+  'somabase': ('https://populse.github.io/soma-base/%s',
+    'somabase '),
+  'somaworkflow': ('https://populse.github.io/soma-workflow/%s',
+    'somaworkflow '),
+  'casadistro': ('https://brainvisa.github.io/casa-distro/%s',
+    'casadistro '),
+}
+
+
+pyversion = '%d.%d' % sys.version_info[:2]
+
+intersphinx_mapping = {
+  'somabase': ('https://populse.github.io/soma-base', None),
+  'somaworkflow': ('https://populse.github.io/soma-workflow', None),
+  'python': ('https://docs.python.org/%s' % pyversion, None),
+  'traits': ('https://docs.enthought.com/traits', None),
+}
+
+
+# -- mermaid configuration ---------------------------------------------------
+
+mermaid_output_format = 'raw'
