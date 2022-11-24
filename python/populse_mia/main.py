34f4066b6bfc759588b689e87b7966d00fc403a4
econdami <econdami@yahoo.fr>
2022-11-24 15:18:43 +0100

    no etelemetry at all

--- python/populse_mia/main.py
+++ python/populse_mia/main.py
@@ -38,8 +38,12 @@ from PyQt5.QtCore import QDir, QLockFile, Qt
 from PyQt5.QtWidgets import (QApplication, QDialog, QPushButton, QLabel,
                              QFileDialog, QVBoxLayout, QHBoxLayout, QLineEdit)
 
-if 'NO_ET' not in os.environ:
-    os.environ['NO_ET'] = "1" # disables any etelemetry check.
+# Disables any etelemetry check.
+if "NO_ET" not in os.environ:
+    os.environ["NO_ET"] = "1"
+
+if "NIPYPE_NO_ET" not in os.environ:
+    os.environ["NIPYPE_NO_ET"] = "1"
 
 # Adding populse_mia path to the sys.path if in developer mode
 if not os.path.dirname(os.path.dirname(
