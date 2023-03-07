9eb381ea739e67a16672217922be1af89c2815ca
econdami <econdami@yahoo.fr>
2023-03-07 11:34:32 +0100

    update documentation

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2.2-dev+e4522b29',
+    VERSION: '2.2.2-dev+5b4caf0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
