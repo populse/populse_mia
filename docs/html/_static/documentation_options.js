0807443c265bd9f83cac7a1ff7766da6d2363efa
econdami <econdami@yahoo.fr>
2023-10-05 12:45:35 +0200

    try to fix github Dependabot alerts on jquery version

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.4.1-dev+6a4a44bd',
+    VERSION: '2.4.1-dev+ca509105',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
