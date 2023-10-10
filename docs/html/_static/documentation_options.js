a52b074268e3e0084b92e97a4f3eb6445e5dc8b4
econdami <econdami@yahoo.fr>
2023-10-10 11:49:16 +0200

    make html

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.4.1-dev+ca509105',
+    VERSION: '2.4.1-dev+932413e7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
