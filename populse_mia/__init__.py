a9c4bc067486c883b3b5c69af1cc04e90f6e46ac
econdami <econdami@yahoo.fr>
2025-02-03 17:02:58 +0100

    Make Mia compatible with populse_db V3 (#17):
            - populse_mia/__init__.py

--- populse_mia/__init__.py
+++ populse_mia/__init__.py
@@ -1,39 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-MIA is shorthand for “Multiparametric Image Analysis”. It is intended to be
-a complete image processing environment mainly targeted at the analysis and
-visualization of large amounts of MRI data.
+MIA (Multiparametric Image Analysis) is a comprehensive image processing
+environment primarily designed for the analysis and visualization of large
+MRI datasets.
 
-MRI data analysis often requires a complex succession of data processing
-pipelines applied to a set of data acquired in an MRI exam or over several
-MRI exams. This analysis may need to be repeated a large number of times in
-studies involving a large number of acquisition sessions. Such that manual
-execution of the processing modules or simple ad-hoc scripting of the
-process may become error-prone, cumbersome and difficult to reproduce. Data
-processing pipelines exist in separate heterogeneous toolboxes, developed
-in-house or by other researchers in the field. This heterogeneity adds to
-the complexity of the modules are to be invoked manually.
+MRI data analysis often requires executing complex processing pipelines on
+datasets acquired during single or multiple MRI exams. In large-scale studies,
+where data processing must be repeated across numerous acquisition sessions,
+manually running processing modules or relying on simple ad-hoc scripts can
+become error-prone, cumbersome, and difficult to reproduce. Additionally, data
+processing pipelines are often distributed across various heterogeneous
+toolboxes, developed either in-house or by other researchers, further
+increasing the complexity of manually invoking these modules.
 
-MIA (populse_mia) aims to provide easy tools to perform complex data processing
-based on a definition of the inputs and outputs of the individual pipelines
-on a conceptual level, and implies identifying data with respect to their
-role in an analysis project: “the scan type”, “the subject being scanned”,
-“the group of this subject is part of”, etc.
+MIA (populse_mia) aims to simplify complex data processing by providing
+intuitive tools that define inputs and outputs at a conceptual level. It
+organizes data based on their roles in an analysis project, such
+as “scan type,” “subject,” or “subject group,” making it easier to structure
+and automate workflows.
 
+Contents:
 
-:Contains:
-    :Packages:
-      - data_manager: handles the projects and their database
-      - sources_images: a collection of images used in Mia
-      - user_interface: mainly intended for the graphic interface of Mia
-      - utils: various utilities for the Mia operation
+  - Packages
 
-    :Modules:
-      - info.py: define software version, description and requirements
-      - main.py: the first module used at the runtime of mia
-      - __main__.py: make populse_mia accessible as a module
-      - software_properties.py: handles the configuration of the software
-      - test.py: unitary tests
+    - data_manager: Manages projects and their associated databases.
+    - sources_images: Contains a collection of images used in MIA.
+    - user_interface: Handles the graphical user interface of MIA.
+    - utils: Provides various utility functions for MIA operations.
+
+  - Modules
+
+    - info.py: Defines software version, description, and requirements.
+    - main.py: The primary module executed at runtime.
+    - __main__.py: Allows populse_mia to be executed as a module.
+    - software_properties.py: Manages the software’s configuration.
+    - test.py: Contains unit tests.
 
 """
 
