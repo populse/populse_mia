cbdb237ec3834923eb6b4d4bdf2ec848da79ce0e
econdami <econdami@yahoo.fr>
2025-02-21 16:33:44 +0100

    Make Mia compatible with populse_db V3 (#17):
    	- populse_mia/sources_images/__init__.py
    	- populse_mia/user_interface/__init__.py
    	- populse_mia/user_interface/main_window.py

--- populse_mia/sources_images/__init__.py
+++ populse_mia/sources_images/__init__.py
@@ -1,1 +1,16 @@
-"""Blabla"""
+"""
+Images Module.
+
+This module contains PNG image resources used throughout Mia application.
+The images are organized within this package to facilitate easy access
+and management.
+
+"""
+
+###############################################################################
+# Populse_mia - Copyright (C) IRMaGe/CEA, 2018
+# Distributed under the terms of the CeCILL license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html
+# for details.
+###############################################################################
