7063cee150154c9ebc795a91886bc197aaafe5e9
econdami <econdami@yahoo.fr>
2025-03-07 21:02:25 +0100

    Make Mia compatible with populse_db V3 (#17):
    	- populse_mia/user_interface/data_browser/__init__.py
    	- populse_mia/user_interface/data_browser/rapid_search.py
    	- populse_mia/user_interface/data_browser/modify_table.py
    	- populse_mia/user_interface/data_browser/count_table.py
    	- populse_mia/user_interface/data_browser/advanced_search.py

--- populse_mia/user_interface/data_browser/modify_table.py
+++ populse_mia/user_interface/data_browser/modify_table.py
@@ -1,5 +1,13 @@
 """
-Module to handle updates of the databrowser table after modifications.
+Database cell editor module for list-type values.
+
+This module provides dialog interfaces for editing complex data types
+in the Mia data browser. It specifically handles the editing of list-type
+values such as arrays of numbers, strings, or dates.
+
+The ModifyTable dialog creates an interactive table representation of lists,
+allowing users to add, edit, or remove items while ensuring type safety
+and database consistency.
 
 Contains:
     Class:
@@ -14,18 +22,8 @@ Contains:
 # for details.
 ##########################################################################
 
+import logging
 from datetime import datetime
-
-# Populse_db imports
-from populse_db.database import (
-    FIELD_TYPE_LIST_BOOLEAN,
-    FIELD_TYPE_LIST_DATE,
-    FIELD_TYPE_LIST_DATETIME,
-    FIELD_TYPE_LIST_FLOAT,
-    FIELD_TYPE_LIST_INTEGER,
-    FIELD_TYPE_LIST_STRING,
-    FIELD_TYPE_LIST_TIME,
-)
 
 # PyQt5 imports
 from PyQt5.QtWidgets import (
@@ -39,212 +37,249 @@ from PyQt5.QtWidgets import (
 )
 
 # Populse_MIA imports
-from populse_mia.data_manager.project import COLLECTION_CURRENT
+from populse_mia.data_manager import (
+    COLLECTION_CURRENT,
+    FIELD_TYPE_LIST_BOOLEAN,
+    FIELD_TYPE_LIST_DATE,
+    FIELD_TYPE_LIST_DATETIME,
+    FIELD_TYPE_LIST_FLOAT,
+    FIELD_TYPE_LIST_INTEGER,
+    FIELD_TYPE_LIST_STRING,
+    FIELD_TYPE_LIST_TIME,
+)
+
+logger = logging.getLogger(__name__)
 
 
 class ModifyTable(QDialog):
-    """Used to modify the contents of cells which contains lists.
+    """
+    Dialog to modify cell contents containing lists in the data browser tab.
 
-    When the user wishes to modify a cell which contains a list in the
-    databrowser tab, a popup will appear to help the user to change the
-    content of the cell.
+    This dialog provides a user interface to edit cells that contain list
+    values. It displays the list elements in a table and allows users to add
+    or remove elements.
 
     .. Methods:
-        - add_item: add one more element to self.value
-        - fill_table: fill the table
-        - rem_last_item: remove last element of self.value
-        - update_table_values: update the table in the database
+        - _convert_value: Convert a text value to the appropriate type
+        - _show_error_message: Display an error message
+        - add_item: Add one more element to self.value
+        - fill_table: Fill the table
+        - rem_last_item: Remove last element of self.value
+        - update_table_values: Update the table in the database
     """
 
     def __init__(self, project, value, types, scans, tags):
         """
-        Initialization of the ModifyTable class
+        Initialize the ModifyTable dialog.
 
-        :param project: Instance of project
-        :param value: List of values of the cell
-        :param types: Value types
-        :param scans: Scans of the rows
-        :param tags: Tags of the columns
+        :param project: Instance of the current project
+        :param value: List of values in the cell to be modified
+        :param types: Allowed value types for validation
+        :param scans: Scan identifiers corresponding to rows
+        :param tags: Tag identifiers corresponding to columns
         """
         super().__init__()
         self.setModal(True)
-
-        # Variables init
+        # Initialize instance variables
         self.types = types
         self.scans = scans
         self.tags = tags
         self.project = project
         self.value = value
-        # The table that will be filled
+        # Create and configure the table
         self.table = QTableWidget()
-
-        # Filling the table
         self.fill_table()
-
-        # Ok button
+        # Create control buttons
         ok_button = QPushButton("Ok")
         ok_button.clicked.connect(self.update_table_values)
-
-        # Cancel button
         cancel_button = QPushButton("Cancel")
         cancel_button.clicked.connect(self.close)
+        # plus_button = QPushButton("+")
+        add_button = QPushButton("+")
+        add_button.setToolTip("Add one more element to the list")
+        add_button.clicked.connect(self.add_item)
+        # minus_button = QPushButton("-")
+        remove_button = QPushButton("-")
+        remove_button.setToolTip("Remove the last element of the list")
+        remove_button.clicked.connect(self.rem_last_item)
+        # Set up layout
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addWidget(ok_button)
+        self.button_layout.addWidget(cancel_button)
+        self.button_layout.addWidget(add_button)
+        self.button_layout.addWidget(remove_button)
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.table)
+        self.main_layout.addLayout(self.button_layout)
+        self.setLayout(self.main_layout)
 
-        # + button (add one more element to a list)
-        plus_button = QPushButton("+")
-        plus_button.setToolTip("Add one more element to the list")
-        plus_button.clicked.connect(self.add_item)
+    def _convert_value(self, text, field_type):
+        """
+        Convert a text value to the appropriate type based on field_type.
 
-        # - button (remove last element from a list)
-        minus_button = QPushButton("-")
-        minus_button.setToolTip("Remove the last element of the list")
-        minus_button.clicked.connect(self.rem_last_item)
+        :param text: String value to convert
+        :param field_type: Database field type constant
 
-        # Layouts
-        self.v_box_final = QVBoxLayout()
-        self.h_box_final = QHBoxLayout()
+        :return: The converted value in the appropriate type
+        """
 
-        self.h_box_final.addWidget(ok_button)
-        self.h_box_final.addWidget(cancel_button)
-        self.h_box_final.addWidget(plus_button)
-        self.h_box_final.addWidget(minus_button)
+        if field_type == FIELD_TYPE_LIST_INTEGER:
+            return int(text)
 
-        self.v_box_final.addWidget(self.table)
-        self.v_box_final.addLayout(self.h_box_final)
+        elif field_type == FIELD_TYPE_LIST_FLOAT:
+            return float(text)
 
-        self.setLayout(self.v_box_final)
+        elif field_type == FIELD_TYPE_LIST_STRING:
+            return str(text)
+
+        elif field_type == FIELD_TYPE_LIST_BOOLEAN:
+            return eval(text)
+
+        elif field_type == FIELD_TYPE_LIST_DATE:
+            return datetime.strptime(text, "%d/%m/%Y").date()
+
+        elif field_type == FIELD_TYPE_LIST_DATETIME:
+            return datetime.strptime(text, "%d/%m/%Y %H:%M:%S.%f")
+
+        elif field_type == FIELD_TYPE_LIST_TIME:
+            return datetime.strptime(text, "%H:%M:%S.%f").time()
+
+    def _show_error_message(self, value, type_problem):
+        """
+        Display an error message for invalid values.
+
+        :param value: The invalid value
+        :param type_problem: The specific type that failed validation
+        """
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setText("Invalid value")
+        msg.setInformativeText(
+            f"The value {value} is invalid with the type {type_problem}"
+        )
+        msg.setWindowTitle("Warning")
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.buttonClicked.connect(msg.close)
+        msg.exec()
 
     def add_item(self):
-        """Add one more element to self.value"""
+        """
+        Add a new element to the list with default value 0.
+        """
         self.value.append(0)
         # Filling the table
         self.fill_table()
 
     def fill_table(self):
-        """Fill the table."""
-        # Sizes
+        """
+        Populate the table with the current list elements.
+
+        Configures the table dimensions, populates cells with values,
+        and adjusts table size to fit content within reasonable bounds.
+        """
+        # Set table dimensions
         self.table.setColumnCount(len(self.value))
         self.table.setRowCount(1)
 
-        # Values filled
-        for i in range(0, self.table.columnCount()):
-            column_elem = self.value[i]
-            item = QTableWidgetItem()
-            item.setText(str(column_elem))
-            self.table.setItem(0, i, item)
+        # Populate cells with values
+        for col, value in enumerate(self.value):
+            item = QTableWidgetItem(str(value))
+            self.table.setItem(0, col, item)
 
-        # Resize
+        # Resize columns to fit content
         self.table.resizeColumnsToContents()
-        total_width = 0
-        total_height = 0
-        i = 0
-
-        while i < self.table.columnCount():
-            total_width += self.table.columnWidth(i)
-            total_height += self.table.rowHeight(i)
-            i += 1
-
-        if total_width + 20 < 900:
-            self.table.setFixedWidth(total_width + 20)
-            self.table.setFixedHeight(total_height + 25)
-
-        else:
-            self.table.setFixedWidth(900)
-            self.table.setFixedHeight(total_height + 40)
+        # Calculate total dimensions
+        total_width = sum(
+            self.table.columnWidth(i) for i in range(self.table.columnCount())
+        )
+        total_height = sum(
+            self.table.rowHeight(i) for i in range(self.table.rowCount())
+        )
+        # Set table dimensions with constraints
+        width = min(total_width + 20, 900)
+        height = total_height + (25 if total_width + 20 < 900 else 40)
+        self.table.setFixedWidth(width)
+        self.table.setFixedHeight(height)
 
     def rem_last_item(self):
-        """Remove last element of self.value"""
+        """
+        Remove the last element of the list if there's more than one element.
+
+        Lists must maintain at least one element.
+        """
+
         if len(self.value) > 1:
             self.value.pop()
             # Filling the table
             self.fill_table()
 
         else:
-            print(
-                "\nThe list must contain at least one element. "
-                "Deletion of the last element is aborted!\n"
+            logger.info(
+                "The list must contain at least one element. "
+                "Deletion of the last element is aborted!"
             )
 
     def update_table_values(self, test=False):
-        """Update the table in the database when the 'OK' button is clicked."""
+        """
+        Validate user input and update the database with new values.
+
+        Validates each value against specified types and updates the database
+        only if all values are valid.
+
+        :param test (bool): Flag for testing mode, defaults to False
+        """
 
         # import check_value_type only here to prevent circular import issue
         from populse_mia.utils import check_value_type
 
         valid = True
 
-        # For each value, type checked
-        for i in range(0, self.table.columnCount()):
-            item = self.table.item(0, i)
+        # Validate each cell value against allowed types
+        for col in range(0, self.table.columnCount()):
+            item = self.table.item(0, col)
             text = item.text()
+            type_problem = None
 
-            valid_type = True
+            # Check if value is valid for each of the allowed types
             for tag_type in self.types:
+
                 if not check_value_type(text, tag_type, True):
-                    valid_type = False
+                    valid = False
                     type_problem = tag_type
                     break
 
-            # Type checked
-            if not valid_type:
-                # Error dialog if invalid cell
-                valid = False
-                if test is False:
-                    msg = QMessageBox()
-                    msg.setIcon(QMessageBox.Warning)
-                    msg.setText("Invalid value")
-                    msg.setInformativeText(
-                        "The value "
-                        + text
-                        + " is invalid with the type "
-                        + type_problem
-                    )
-                    msg.setWindowTitle("Warning")
-                    msg.setStandardButtons(QMessageBox.Ok)
-                    msg.buttonClicked.connect(msg.close)
-                    msg.exec()
-                break
+            # If not valid, show error and stop validation
+            if not valid:
 
-        if valid:
-            # Database updated only if valid type for every cell
-            for cell in range(0, len(self.scans)):
-                scan = self.scans[cell]
-                tag = self.tags[cell]
-                tag_object = self.project.session.get_field(
+                if not test:
+                    self._show_error_message(text, type_problem)
+
+                return
+
+        # Only update database if all values are valid
+        # Update database for each cell with validated values
+
+        with self.project.database.data() as database_data:
+
+            for scan, tag in zip(self.scans, self.tags):
+                # Get field attributes
+                tag_attrib = database_data.get_field_attributes(
                     COLLECTION_CURRENT, tag
                 )
-                tag_type = tag_object.field_type
-
+                tag_type = tag_attrib["field_type"]
+                # Convert values according to field type
                 database_value = []
 
-                # For each value
-                for i in range(0, self.table.columnCount()):
+                for i in range(self.table.columnCount()):
                     item = self.table.item(0, i)
                     text = item.text()
+                    database_value.append(self._convert_value(text, tag_type))
 
-                    if tag_type == FIELD_TYPE_LIST_INTEGER:
-                        database_value.append(int(text))
-                    elif tag_type == FIELD_TYPE_LIST_FLOAT:
-                        database_value.append(float(text))
-                    elif tag_type == FIELD_TYPE_LIST_STRING:
-                        database_value.append(str(text))
-                    elif tag_type == FIELD_TYPE_LIST_BOOLEAN:
-                        database_value.append(eval(text))
-                    elif tag_type == FIELD_TYPE_LIST_DATE:
-                        format = "%d/%m/%Y"
-                        subvalue = datetime.strptime(text, format).date()
-                        database_value.append(subvalue)
-                    elif tag_type == FIELD_TYPE_LIST_DATETIME:
-                        format = "%d/%m/%Y %H:%M:%S.%f"
-                        subvalue = datetime.strptime(text, format)
-                        database_value.append(subvalue)
-                    elif tag_type == FIELD_TYPE_LIST_TIME:
-                        format = "%H:%M:%S.%f"
-                        subvalue = datetime.strptime(text, format).time()
-                        database_value.append(subvalue)
-
-                # Database updated for every cell
-                self.project.session.set_value(
-                    COLLECTION_CURRENT, scan, tag, database_value
+                # Update database
+                database_data.set_value(
+                    collection_name=COLLECTION_CURRENT,
+                    primary_key=scan,
+                    values_dict={tag: database_value},
                 )
 
-            self.close()
+        self.close()
