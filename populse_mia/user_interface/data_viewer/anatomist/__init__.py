01f682521ab839c1789842f63a744ba4cb928c20
econdami <econdami@yahoo.fr>
2025-03-28 19:00:36 +0100

    Make Mia compatible with populse_db V3 (#17):
    	- populse_mia/user_interface/data_viewer/anatomist/__init__.py
    	- populse_mia/user_interface/data_viewer/anatomist/mia_anatomist.py

--- populse_mia/user_interface/data_viewer/anatomist/__init__.py
+++ populse_mia/user_interface/data_viewer/anatomist/__init__.py
@@ -1,3 +1,14 @@
-"""Blabla"""
+"""
+This is the original viewer based on Anatomist
+(anatomist.simpleviewer.anasimpleviewer).
+"""
+
+##########################################################################
+# Populse_mia - Copyright (C) IRMaGe/CEA, 2018
+# Distributed under the terms of the CeCILL license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html
+# for details.
+##########################################################################
 
 from .mia_anatomist import MiaViewer  # noqa: F401
